051a7f86db4695c2ab6be65a8b72a3bb85e5ef2f
Administrator <Administrator@6SYKVV4VAB4N3IZ>
2020-04-01 10:49:21 +0800

    ffffffffffffff

---
+++ webDemo/src/main/java/com/navy/business/testGenerator/dao/UserMapper.java
@@ -0,0 +1,22 @@
+package com.navy.business.testGenerator.dao;
+
+import com.navy.business.testGenerator.model.User;
+import com.navy.business.testGenerator.model.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+}
---
+++ webDemo/src/test/java/com/navy/WebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
